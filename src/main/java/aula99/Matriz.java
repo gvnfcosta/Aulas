e46d234a13cec93d5bba519a791062501442f91b
Giovanni Costa <gvnfcosta@gmail.com>
2021-09-06 16:55:04 -0300

    Entra com números inteiros numa matriz, exibe os números na diagonal e depois exibe os números negativos da matriz se tiver. Aula 99 do curso de Java da udemy

---
+++ src/main/java/aula99/Matriz.java
@@ -0,0 +1,40 @@
+package aula99;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Matriz {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Qual a dimensão da matriz? ");
+        int n = sc.nextInt();
+
+        int[][] matriz = new int[n][n];
+
+        for (int i=0; i<matriz.length; i++){
+            for (int j=0; j<matriz.length; j++){
+                System.out.print("Entre com um número: ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Numeros diagonais");
+        for (int i=0; i<matriz.length; i++){
+            System.out.print(matriz[i][i] + " ");
+        }
+
+        System.out.println();
+        System.out.println("Numeros negativos");
+        for (int i=0; i<matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                if (matriz[i][j] < 0) {
+                    System.out.print(matriz[i][j] + " ");
+                }
+            }
+        }
+        sc.close();
+    }
+}
